a92818578b8e51a050060c9732a722cac80507a6
Kevin <mostert.k@gmail.com>
2019-03-29 10:09:48 +0200

    Version 1 final touches
    Still need to fix the backing up of Directories and maybe add some color to the outputs.

--- archive.sh
+++ archive.sh
@@ -6,10 +6,10 @@
 # 25/03/2019
 
 #Test to make sure that arguments are entered.
-TYPETEST="$1""$2""$3"
+TYPETEST="$@"
 if [ ! -n "$TYPETEST" ]
 then
-	echo 'Please specify at least one file or directory to backup.'
+	echo 'Please specify at least one file to backup.'
 	exit 1
 fi
 
@@ -40,12 +40,12 @@ do
 	#Non-compression Archiving
 	if [[ $has_n_option = true && $has_r_option = false ]]
 	then
-		echo "Non-compression backup of "$i" started."
+		echo "Non-compression backup of "$i" started..."
 		#File exists and string is non-zero and doesnt have a .bck* extension
 		if [ -e "$i" ] && [ ! -z "$i" ] && [[ $i != *.bck* ]]
 		then
 			cp -i "$i" "$i".bck
-			echo "NON-COMPRESSED - Archive of "$i" in "$location" completed on $(date)" | tee -a /home/"$USER"/scripts.log
+			echo "NON-COMPRESSED - Archive of "$i" in "$location"/ completed on $(date)." | tee -a /home/"$USER"/scripts.log
 		#File doesn't exist and the string is non-zero
 		elif [ ! -e "$i" ] && [ ! -z "$i" ]
 		then
@@ -62,38 +62,36 @@ do
 	# Compression Archiving
 	if [[ $has_n_option == false ]] && [[ $has_r_option == false ]]
 	then
-		echo "Compression backup of "$i" started."
-		# If file* exists and string is not zero and the file isnt a backup and there is no *.bck* in directory already
+		echo "Compression backup of "$i" started..."
+		# If file exists and string is not zero and the file isnt a backup and there is no *.bck* in directory already
 		if [[ -e $i ]] && [[ ! -z $i ]] && [[ $i != *.bck* ]] && [[ ! -e $i.bck.gz ]] && [[ ! -e $i.bck ]]
 		then
-			echo "A"
 			cp "$i" "$i".bck
 			gzip "$i".bck
-			echo "COMPRESSED - Archive of "$i" in "$location" completed on $(date)" | tee -a /home/"$USER"/scripts.log
+			echo "COMPRESSED - Archive of "$i" in "$location"/ completed on $(date)." | tee -a /home/"$USER"/scripts.log
 			gzip -l "$i".bck.gz >> /home/"$USER"/scripts.log
 
-		# If file* exists and string is not zero and file.bck* already exist
+		# If file exists and string is not zero and file.bck* already exist
 		elif [[ -e $i ]] && [[ ! -z $i ]] && [[ -e $1.bck || -e $1.bck.gz ]]
 		then
-			echo "B1"
+			echo "A backup file of "$i" already exists "
 			if [[ -e $i.bck ]]
 			then
-				echo "B1.1"
-				read -r -p "Since you are compressing "$i", do you want to remove "$i".bck [Y/N]? " response
+				read -r -p "Since you are compressing "$i", do you want to remove the existing "$i".bck file [Y/N]? " response
 				if [[ $response =~ [yY(es)*] ]]
 				then
+					echo ""$1".bck was removed."
 					cp "$i" "$i".bck
 					gzip -f "$i".bck
-					echo "COMPRESSED - Archive of "$i" completed on $(date)" | tee -a /home/"$USER"/scripts.log
+					echo "COMPRESSED - Archive of "$i" to "$location"/ completed on $(date)." | tee -a /home/"$USER"/scripts.log
 					gzip -l "$i".bck.gz >> /home/"$USER"/scripts.log
-					echo ""$1".bck was removed"
 				else
+					echo ""$i".bck was preserved."
 					cp "$i" "$i".bckz
 					gzip "$i".bckz
 					mv "$i".bckz.gz "$i".bck.gz
-					echo "COMPRESSED - Archive of "$i" completed on $(date)" | tee -a /home/"$USER"/scripts.log
+					echo "COMPRESSED - Archive of "$i" to "$location"/ completed on $(date)." | tee -a /home/"$USER"/scripts.log
 					gzip -l "$i".bck.gz >> /home/"$USER"/scripts.log
-					echo ""$i".bck was preserved."
 				fi
 			fi
 
@@ -117,22 +115,22 @@ for i in "$@"
 do
 	if [[ $has_r_option = true ]] && [[ $has_n_option = false ]]
 	then
-		echo "Un-Archiving $i"
+		#echo "Un-Archiving $i"
 		if [[ -e $i ]] && [[ "$i" = *.bck ]]
 		then
-			echo "Resoring Non-Compressed File - "$i""
+			echo "Restoring Non-Compressed File - "$i""
 			base=$(basename "$i" .bck)
 			location=$(pwd "$i")
-			mv "$i" "$location"/"$base"
+			mv "$i" "$base"
 			echo "Un-Archive of "$i" to "$location/" , completed on $(date)" | tee -a /home/"$USER"/scripts.log
 		elif [[ -e $i ]] && [[ "$i" = *.bck.gz ]]
 		then
-			echo ""$i" - Compressed File"
-			gzip -d "$i"
+			echo "Restoring Compressed File - "$i""
+			gzip -df "$i"
 			base=$(basename "$i" .bck.gz)
 			location=$(pwd "$i")
 			mv $(basename "$i" .gz) "$location"/"$base"
-			echo "Un-Archive of "$i" to "$location"/"$base" , completed on $(date)" | tee -a /home/"$USER"/scripts.log
+			echo "Un-Archive of "$i" to "$location"/ , completed on $(date)" | tee -a /home/"$USER"/scripts.log
 		elif [[ -e $i ]] && [[ "$i" != *.bck || "$i" != *.bck.gz ]]
 		then
 			echo ""$i" doesnt seem to be a supported file! Please choose "$i".bck or "$i".bck.gz"
@@ -145,25 +143,25 @@ do
 	fi
 done
 
-
 ###	TESTING AREA ##########
-echo "Validators"
-echo '#################################'
+
+#echo "Validators"
+#echo '#################################'
+
 #if [[ $1 == *bck ]]
 #then
 #	echo ""
-#	echo "$1 is a backup file with $base as base"
+#	echo ""$1" is a backup file with "$base" as base"
 #elif [[ $1 != *.bck ]]
 #then
 #	echo ""
-#	echo "$1 is NOT a backup file"
+#	echo ""$1" is NOT a backup file"
 #fi
 
-#echo ""
 #echo "Base1:$base_file1"
 #echo "Base2:$base_file2"
 #echo "$location/$base"
-echo "Filei: "$i""
+#echo "Filei: "$i""
 #echo "File1: $1"
 #echo "File2: $2"
 #echo "File3: $3"
